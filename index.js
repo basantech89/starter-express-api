77ddecf59a82940499aae6432f57e8079a69d558
Basant Soni <basantsoni@Velotios-MacBook-Air-6.local>
2023-10-22 13:40:45 +0530

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
-const app = express()
-
+const cors = require('cors')
 const helmet = require("helmet")
+
+const app = express()
+app.use(cors())
+
+// helmet.contentSecurityPolicy({
+//   useDefaults: true,
+//   directives: {
+//     "font-src": ["'self'", "external-website.com"],
+//     // allowing styles from any website
+//     "style-src": null,
+//   },
+// })
+
+helmet.hsts({
+    // 60 days
+    maxAge: 86400,
+    // removing the "includeSubDomains" option
+    includeSubDomains: false,
+})
 
 app.use(helmet())
 
